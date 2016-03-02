0d461cb15daa0aaf51cba33b2e5b2de6e6115a5a
Nil Panés <nil.panes@gmail.com>
2016-03-02 21:19:51 +0100

    Mal programao

--- Hola.java
+++ Hola.java
@@ -1,7 +1,7 @@
 public class Hola {
   
-  static String nombre = "UN TIO";
-
+  static String nombre = "UN TIO 2";
+// EL Javi es un mal programador
   public static void main (String[] args){
     System.out.println("Hola, " + nombre);
 
