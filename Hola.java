1c8270414b8a3d2c3b934b67043cb5a590cfda70
Nil Panés <nil.panes@gmail.com>
2016-03-02 21:22:10 +0100

    Lo del javi bien hecho

--- Hola.java
+++ Hola.java
@@ -1,9 +1,11 @@
 public class Hola {
   
-  static String nombre = "UN TIO 2";
+  String nombre = "UN TIO 2";
 // EL Javi es un mal programador
   public static void main (String[] args){
-    System.out.println("Hola, " + nombre);
+	Hola h = new Hola();
+	
+    System.out.println("Hola, " +h.nombre);
 
     
 
