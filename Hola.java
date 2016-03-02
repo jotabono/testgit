a3c26c799d232f4d3ae14cdf70871a35aa52bb90
Javier Bono <jotabono92@gmail.com>
2016-03-02 18:19:50 +0100

    Ahora versión parametrizada

--- Hola.java
+++ Hola.java
@@ -1,5 +1,5 @@
 public class Hola {
-public static void main (String[] args){
-System.out.println("Hola que tal");
-}
+   public static void main (String[] args){
+    System.out.println("Hola, " + args[0]);
+  }
 }
