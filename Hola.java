695dfa9fe9ce25c25f0875f2e19a37526f7e410f
Javier Bono <jotabono92@gmail.com>
2016-03-02 20:25:34 +0100

    Comentario añadido

--- Hola.java
+++ Hola.java
@@ -1,7 +1,7 @@
 public class Hola {
   
-  static String nombre = "UN TIO"; 
-
+  static String nombre = "UN TIO";
+  // Esto es un comentario
   public static void main (String[] args){
     System.out.println("Hola, " + nombre);
 
