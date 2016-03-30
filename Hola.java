4d7325a19291b2b40020ac564c8aa2c16f6947de
Nil Panés <nil.panes@gmail.com>
2016-03-30 20:23:10 +0200

    Bien

--- Hola.java
+++ Hola.java
@@ -1,11 +1,12 @@
 public class Hola {
   
   String nombre = "UN TIO 3";
+	static int edad = 21;
 // EL Javi es un mal programador
   public static void main (String[] args){
 	Hola h = new Hola();
 	
-    System.out.println("Hola, " +h.nombre);
+    System.out.println("Hola, " +h.nombre + edad);
 
     
 
